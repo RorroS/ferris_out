c03cb5313904b9a3d742e1d5dc9a8c14c4e52005
Robin Sliwa <rorrosliwa@gmail.com>
2019-11-06 14:00:56 +0100

    Add ball struct

--- src/main.rs
+++ src/main.rs
@@ -7,32 +7,44 @@ use ggez::event::{KeyCode, KeyMods};
 
 use std::env;
 use std::path;
+use std::vec;
 
 const WINDOW_SIZE: (f32, f32) = (800.0, 800.0);
 
+struct Ball {
+    image: graphics::Image,
+    pos_x: f32,
+    pos_y: f32,
+    speed: f32,
+    dir_x: f32,
+    dir_y: f32,
+}
+
+impl Ball {
+    fn new(ctx: &mut Context) -> Ball {
+        Ball {
+            image: graphics::Image::new(ctx, "/ballis.png").unwrap(),
+            pos_x: 100.0,
+            pos_y: 100.0,
+            speed: 2.5,
+            dir_x: 1.0,
+            dir_y: 0.0,
+        }
+    }
+}
+
 struct MainState {
-    ballis_img: graphics::Image,
     paddle: graphics::Rect,
-    ballis_pos_x: f32,
-    ballis_pos_y: f32,
-    ballis_speed: f32,
-    ballis_dir: f32,
     paddle_pos_x: f32,
     paddle_pos_y: f32,
     paddle_speed: f32,
     paddle_length: f32,
     paddle_height: f32,
+    balls: Vec<Ball>,
 }
 
 impl MainState {
     fn new(ctx: &mut Context) -> GameResult<MainState> {
-        let ballis_img = graphics::Image::new(ctx, "/ballis.png")?;
-
-        let ballis_pos_x = 100.0;
-        let ballis_pos_y = 100.0;
-        let ballis_speed = 2.5;
-        let ballis_dir = 1.0;
-
         let paddle_speed = 5.0;
         let paddle_length = 50.0;
         let paddle_height = 10.0;
@@ -41,18 +53,16 @@ impl MainState {
 
         let paddle = graphics::Rect::new(0.0, 0.0, paddle_length, paddle_height);
 
+        let balls = vec![Ball::new(ctx)];
+
         let s = MainState {
-            ballis_img,
             paddle,
-            ballis_pos_y,
-            ballis_pos_x,
-            ballis_speed,
-            ballis_dir,
             paddle_pos_x,
             paddle_pos_y,
             paddle_speed,
             paddle_length,
             paddle_height,
+            balls,
         };
 
         Ok(s)
@@ -62,21 +72,21 @@ impl MainState {
 impl event::EventHandler for MainState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
         // 26 is height of titlebar.
-        if self.ballis_pos_y + f32::from(self.ballis_img.height()) > graphics::size(ctx).1 - 26.0 {
-            self.ballis_pos_y = 100.0;
-            self.ballis_dir = 1.0;
-        }
-        else if self.ballis_pos_y <= 0.0 {
-            self.ballis_dir = 1.0;
-        }
-        else if self.ballis_pos_y + f32::from(self.ballis_img.height()) > self.paddle_pos_y &&
-            self.ballis_pos_y + f32::from(self.ballis_img.height()) < self.paddle_pos_y + self.paddle_height &&
-            (self.ballis_pos_x + f32::from(self.ballis_img.width()) > self.paddle_pos_x &&
-            self.ballis_pos_x + f32::from(self.ballis_img.width()) < self.paddle_pos_x + self.paddle_length || self.ballis_pos_x > self.paddle_pos_x && self.ballis_pos_x < self.paddle_pos_x + self.paddle_length) {
-            self.ballis_dir = -1.0;
-        }
-
-        self.ballis_pos_y = self.ballis_pos_y % graphics::size(ctx).1 + self.ballis_speed * self.ballis_dir;
+//        if self.ballis_pos_y + f32::from(self.ballis_img.height()) > graphics::size(ctx).1 - 26.0 {
+//            self.ballis_pos_y = 100.0;
+//            self.ballis_x_dir = 1.0;
+//        }
+//        else if self.ballis_pos_y <= 0.0 {
+//            self.ballis_x_dir = 1.0;
+//        }
+//        else if self.ballis_pos_y + f32::from(self.ballis_img.height()) > self.paddle_pos_y &&
+//            self.ballis_pos_y + f32::from(self.ballis_img.height()) < self.paddle_pos_y + self.paddle_height &&
+//            (self.ballis_pos_x + f32::from(self.ballis_img.width()) > self.paddle_pos_x &&
+//            self.ballis_pos_x + f32::from(self.ballis_img.width()) < self.paddle_pos_x + self.paddle_length || self.ballis_pos_x > self.paddle_pos_x && self.ballis_pos_x < self.paddle_pos_x + self.paddle_length) {
+//            self.ballis_x_dir = -1.0;
+//        }
+//
+//        self.ballis_pos_y = self.ballis_pos_y % graphics::size(ctx).1 + self.ballis_speed * self.ballis_x_dir;
 
         if ggez::input::keyboard::is_key_pressed(ctx, KeyCode::Left) {
             if self.paddle_pos_x - self.paddle_speed >= 0.0 {
@@ -95,9 +105,14 @@ impl event::EventHandler for MainState {
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
 
-        graphics::draw(ctx, &self.ballis_img,
-                       (Point2::new(self.ballis_pos_x, self.ballis_pos_y),)
-        )?;
+        for ball in &self.balls {
+            graphics::draw(ctx, &ball.image, (Point2::new(ball.pos_x, ball.pos_y),));
+        }
+
+       // let balls_to_the_walls = Ball::new(ctx);
+       // graphics::draw(ctx, &balls_to_the_walls.image,
+       //                (Point2::new(balls_to_the_walls.pos_x, balls_to_the_walls.pos_y),)
+       // )?;
 
         let paddle_mesh = graphics::Mesh::new_rectangle(
             ctx,
